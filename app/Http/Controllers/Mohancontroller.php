cdd67af02ce7eb3719e1239be180250aaa9c8f57
mohan khulal <khulalmohan712@gmail.com>
2024-07-23 09:32:52 +0545

    table

---
+++ app/Http/Controllers/Mohancontroller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Mohancontroller extends Controller
+{
+    public function table (){
+        return view ("table");
+
+    }
+}
+
+
